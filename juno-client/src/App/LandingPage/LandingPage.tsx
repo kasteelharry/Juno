d752b2a68280748b053172faedefda640681fccc
Joris Kuiper <joriskuiper2@gmail.com>
2022-09-24 22:41:54 +0200

    Added welcome message

--- juno-client/src/App/LandingPage/LandingPage.tsx
+++ juno-client/src/App/LandingPage/LandingPage.tsx
@@ -1,4 +1,4 @@
-import { Box } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import SocialButtons from '../Components/SocialButtons';
 import "./LandingPage.scss"
 
@@ -9,9 +9,22 @@ function LandingPage() {
 
     return (
         <Box className='LandingPage'>
-            
-            <SocialButtons/>
-
+            <Box className='TitleAndLinks'>
+                <Typography
+                            variant="h3"
+                            noWrap
+                            fontWeight='bold'
+                            component="div"
+                            sx={{ml:3, mr: 2, paddingBottom:3,
+                                display: { xs: 'none', md: 'flex' }, fontFamily:"Segoe UI"}}
+                        >
+                            Hi, I am Joris Kuiper! <br/>
+                            I program stuff for fun<br/>
+                            and try new things. <br/>
+                </Typography>
+                <SocialButtons/>
+            </Box>
+            {/* TODO Add circular photo to the right of the box that is above this in the code. */}
         </Box>
     );
 }

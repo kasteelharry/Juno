1cc32449bd5e9e34f4c3fd9a54d3f59279009038
Joris Kuiper <kuiperjs@inter-actief.net>
2023-07-29 17:47:07 +0200

    Added redirects for Norway

--- juno-api/next.config.js
+++ juno-api/next.config.js
@@ -2,6 +2,22 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async redirects() {
+    return [
+      {
+        source: '/hardangervidda',
+        destination: 'https://share.garmin.com/JorisHardangervidda',
+        permanent: false,
+        basePath: false
+      },
+      {
+        source: '/norway',
+        destination: 'https://share.garmin.com/JorisHardangervidda',
+        permanent: false,
+        basePath: false
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
